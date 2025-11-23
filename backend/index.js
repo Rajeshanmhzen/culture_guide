5610095b80b7c6f53efef2e2f9e777bec2345365
Rajeshan Maharjan <rajeshanmaharjan.25@gmail.com>
2025-11-23 19:28:17 +0545

    adding the frontend in the cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser())
 
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: process.env.frontend_URL,
     credentials:true,
     exposeHeaders: ['Access-Control-Allow-Credentials'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
